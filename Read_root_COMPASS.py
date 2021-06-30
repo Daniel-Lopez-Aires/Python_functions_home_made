a189fc4f61101eca6bf71eb29b0c2df06ce0aa00
Daniel Lopez Aires <64276735+Daniel-Lopez-Aires@users.noreply.github.com>
2021-06-30 15:28:17 +0200

    Included function to read .root with all channels, so 2 functions here!!!
    Huge changes:
    *Have included another function, to read the .root containng all the channels, so the same file now contains the 2 functions to read .roots from COMPASS
    *From the .root with all the channels, storing the energy histogram spectra

--- Read_root_COMPASS.py
+++ Read_root_COMPASS.py
@@ -5,23 +5,6 @@ Created on Tue Jun 29 09:23:48 2021
 
 @author: Daniel López Aires// danlopair@gmail.com
 
-This function is to read .root data from COMPASS (filtered data, which is the one 
-with the good data), in order to capture the wave, or what we think it should be 
-the wave. .root better than .csv because it is approx an order of magnitude lighter
-(10MB vs 100MB)
-
-*Inputs:
-        .name = filename in string format, eg: 'blablabla.root'
-        
-*Outputs:
-        .Channel = channel of the peak in the histogram
-        .Board channel = board channel, the ch of the digitizer used (usually 15)
-        .waveform = vector data with the waveform in strange units.
-	.time = vector data for the waveform plot
-	.Flags, Timestamp = vectors given by the.root, but useless for us
-	
-#Biblio: https://root.cern.ch/doc/master/pyroot002__TTreeAsMatrix_8py.html
-        https://root-forum.cern.ch/t/unable-to-read-a-ttree-with-leaves-and-a-branch-tarrays-with-pyroot/45578
 """
 
 
@@ -39,8 +22,36 @@ from ROOT import TPad, TPaveLabel, TTreeReader
 #############
 
 
-def ReadRootCOMPASS(name):
-    
+#%%  ###############################################################
+######## 1) Function to read .root files containing a single channels######
+####################################################################
+
+
+def ReadRootSingleCOMPASS(name):
+
+    """
+This function is to read .root data from COMPASS (filtered data, which is the 
+one  with the good data) if storing each channel in a single .root.
+.root better than .csv because it is approx an order of magnitude lighter 
+(10MB vs 100MB). Note that to obtain the wave, each channel have to be saved
+in a .root, and the .root weights an order of magnitude more (8MB vs 200kB)
+
+*Inputs:
+        .name = filename in string format, eg: 
+                'DataF_CH14@V1725S_646_run.root'
+        
+*Outputs:
+        .Channel = channel of the peak in the histogram
+        .Board channel = board channel, the ch of the digitizer used (usually 15)
+        .waveform = vector data with the waveform in strange units.
+	.time = vector data for the waveform plot
+	.Flags, Timestamp = vectors given by the.root, but useless for us
+	
+#Biblio: https://root.cern.ch/doc/master/pyroot002__TTreeAsMatrix_8py.html
+        https://root-forum.cern.ch/t/unable-to-read-a-ttree-with-leaves-and-a-branch-tarrays-with-pyroot/45578
+        """ 
+
+
     data_root = TFile(name)                          #reading the .root file
     tree=data_root.Data_F                   #getting the Tree
     #Tree.Print()                            #print of the Tree
@@ -50,7 +61,7 @@ def ReadRootCOMPASS(name):
 #contain a branche, that inside it have more leaves.
 
 
-######### 1)Read only specific branches #############
+######### 1.1)Read only specific branches #############
 #There is a simple function called AsMatrix, but since it will be removed,
 #will not use it. eg of that function:
         #E = Tree.AsMatrix(columns=["Energy"])
@@ -97,14 +108,95 @@ def ReadRootCOMPASS(name):
             #the waveform sample    
         
 
-   ########### 2) Return of values ############################
+   ########### 1.2) Return of values ############################
    
    #the values will be returned in a dictionary indicating what is each
    #value
-    values = {'Board_ch' : board, 'E (ch)' : E, 
+    values = {'Board_ch' : board, 'E[ch]' : E, 
               'Voltage_wave[ch]': waveform, 'Timestamp' : timestamp,
               'Ch_digi' : ch_digi, 'Flags': flags,
               'Time_wave[ns]': time
               }
               
     return values          
+
+#%%  ###############################################################
+####### 2) Function to read .root files containing all the channels######
+####################################################################
+
+def ReadRootFullCOMPASS(name):
+
+    """
+This function is to read .root data from COMPASS (filtered data, which is the 
+one  with the good data) if storing all the channels in a single .root.
+.root better than .csv because it is approx an order of magnitude lighter 
+(10MB vs 100MB). Note that to obtain the wave, each channel have to be saved
+in a .root, and the .root weights an order of magnitude more (8MB vs 200kB)
+
+*Inputs:
+        .name = filename in string format, eg: 
+                'HcompassF_run_20210630_095907.root'
+        
+*Outputs:
+        .Counts = counts of all the energy histograms, by columns:
+                counts of hist_0; counts of hist-1; etc
+        .Channels = vector array with the channels, from 0 to the max
+        .n_Channels = number of channels
+	
+        """ 
+
+    data_root = TFile(name)                          #reading the .root file
+    E_folder = data_root.Energy        #folder that contains the 
+                        #Energy histograms
+
+    #Now, the 16 (from 0 to 15) histograms will be obtained, getting the
+    #energy histogram data, the one we are interested in (for the moment).
+    #To avoid importing 1 hist, and then copy paste that 16 times, will create
+    #a loop. To do so, I need to change, in the loop, the name of the hist
+    #to be obtained, which can be done easily:
+    
+    
+    aux_1 = '_F_EnergyCH'                   #the hist name, for the loop,1
+    aux_2 = '@V1725S_646'                   #the hist name, for the loop,2
+    
+    c = np.array( [] )          #Store of the Counts of the energy hist  
+    
+    for i in range(0,16): #loop for each hist, from 0 to 15
+        
+        H = E_folder.Get(aux_1+str(i)+aux_2)  #load of histogram
+        n_ch = H.GetNcells()    #4097, the number of channels in the hist
+        ch = np.linspace(1,n_ch, n_ch)      #channel linspace, for plotting   
+    
+        #To get the bin value of each bin, we need to do a loop, and for each 
+        #iteration, use GetBinContent():
+    
+
+        
+        #Since I do not know how to automatically store things in columns,
+        #I could do the following, which works, first use append, then 
+        #columnstack
+        
+        if i==0:        #1st way to store things (clear variable), 
+                            #so using append
+            for j in range(0, n_ch):   #loop through all the bins
+            
+                c = np.append(c,H.GetBinContent(j))
+                
+        else:   #rest of the cases, using columnstack
+            c_aux = np.array( [] ) #auxiliar variable
+            
+            for j in range(0, n_ch):   #loop through all the bins
+                c_aux = np.append(c_aux, H.GetBinContent(j))   
+            
+            c= np.column_stack((c, c_aux ))      #store in a column; once
+            #we have computed the full new column with c_aux, store it
+
+   ########### 2.2) Return of values ############################
+   
+   #the values will be returned in a dictionary indicating what is each
+   #value
+    values = {'Counts' : c, 'Channels' : ch, 
+              'n_Channels': n_ch
+              }
+              
+    return values   

99db27acca678053057ef416709f6d453d5f9ad9
Daniel Lopez Aires <64276735+Daniel-Lopez-Aires@users.noreply.github.com>
2023-03-20 10:09:50 +0100

    1st commit
    FUnctions to read outputs from experimental measurement devices (XRD, etc)

---
+++ Read_JRC.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 10:29:58 2023
+
+@author: lopedan
+
+This script will contain all the functions I create to read files from experimental measurements,
+say TGA, XRD, etc (the ones that needed ofc)
+"""
+
+######################################
+#%% ######### 0) General packages ###########
+######################################
+
+
+import matplotlib.pyplot as plt  #for simplicity, to not write matplotlib.pyplot
+        #everytime we want to plot something
+#from scipy.stats import norm               ##norm.fit() fit to gaussian
+import numpy as np
+    #np contain linspaces as np.linspace(a,b,N)
+import pandas as pd
+import sys                   #to import functions from other folders!!
+sys.path.insert(0, '//net1.cec.eu.int/jrc-services/KRU-Users/lopedan/Desktop/PhD_Residuos_nucleares/Python/Functions')   
+                                    #path where I have the functions
+import Fits, Peak_analyis_spectra
+
+
+######################################
+#%% ########## TGA reader ############ 
+######################################
+
+
+def Read_TGA (name):
+    '''
+    Function that reads the .txt file from TGA/DTA (HeHeHe), returning a dataframe with the
+    relevant data (omitting intro)
+    '''
+    with open(name) as file_obj:
+        lines = file_obj.readlines()
+        #print('the number of lines of the document is',len(lines))
+        '''
+        From row 1 to 38 there is info about the run. Line 39 has the header, and then the data. Sio
+        with a loop we could get the values
+        '''
+        T = np.array([])       #[°] storage of T values
+        t = np.array([])       #[min] storage of t values
+        DTA  = np.array([])       #[uV/mg] storage of TDA
+        m  = np.array([])       #[%] storage of mass values
+        sens  = np.array([])       #[uV/mW] storage of sensitivity values
+        seg  = np.array([])       # storage of segment values
+
+        #Now the loop to store the values:
+        for i in range(39, len(lines)-1):   
+                #-1 because last line is empty!
+            T = np.append(T, float(lines[i].split(';')[0]) )
+            t = np.append(t, float(lines[i].split(';')[1]) )
+            DTA = np.append(DTA, float(lines[i].split(';')[2]) )
+            m = np.append(m, float(lines[i].split(';')[3]) )
+            sens = np.append(sens, float(lines[i].split(';')[4]) )
+            seg = np.append(seg, float(lines[i].split(';')[5]) )
+
+        #Now lets create the dictionary that will be converted to a df:
+            dic = {'T[°]': T, 't[min]' : t, 'DTA[uV/mg]' : DTA, 'm[%]' : m, 
+           'sens[uV/mW]' : sens, 'seg' : seg}
+            df = pd.DataFrame.from_dict( dic )          #final dataframe
+            
+            
+        #Finally, the return of the relevant values
+        return df
+    
+    
+ 
+######################################
+#%% ########## XRD reader ############ 
+######################################    
+ 
+def Read_XRD_WB (name):
+    '''
+    Function that reads the .dat file from XRD in F141 (W Bonani), returning a df
+    with the relevant info
+    '''
+    with open(name) as f_obj:
+        lines = f_obj.readlines()       # to read all the lines, 1 by 1, and store them
+        #
+        '''
+    The next step is to sepparate the lines and get the values. 1st 2 lines are info,
+    so could be discarded. THe rest, if we split by ' ' (space) we have several blanks ('') 
+    and the 3 numbers (the sepparator, the amount of spaces is variable, so we need to do that).
+    With if statements we can select the numbers (non blanks) store the 3 of them, and we know 
+    that in order they are Q, ExpIntensity and Experror.
+        '''
+    #Initialization variables to store
+        Q = np.array( [] )
+        I = np.array( [] )
+        E = np.array( [] )
+    
+        for i in range(2, len(lines)):      #looping through all lines
+            aux =  lines[i].split(' ')          #variable containing the line splitted
+            #NOw we read element by element that file
+            aux_2 = np.array([])            #varibale storing the 3 numbers
+        
+            for element in aux:
+                if element != '':       #if element different from blank (''), do this
+                    aux_2 = np.append(aux_2, float(element) )       #storing the values
+        
+            #Now I can store the values easily
+            Q = np.append( Q, aux_2[0] )
+            I = np.append( I, aux_2[1])
+            E = np.append( E, aux_2[2] )
+        
+        #Now lets create a dictionary to create a dataframe:
+        dic = {'Q' : Q, 'I' : I, 'Delta_I' : E}
+        df = pd.DataFrame.from_dict( dic)
+        #
+        #Finally return the df:
+        return df
